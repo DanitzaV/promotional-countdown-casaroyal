65884f6420e7c4c5c2e6afef85b29657a344e761
Viviana Marcelo <vivianamarcelo@MacBook-Pro-de-Viviana.local>
2023-03-02 09:57:09 -0300

    creation of product card component edited from site editor

---
+++ react/ProductCard.tsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState, cloneElement, ReactElement } from "react";
+import { useCssHandles } from "vtex.css-handles";
+import "./typings/storefront";
+
+const CSS_HANDLES = [
+  "productContainer",
+  "productContextText",
+  "productText",
+  "productbrand",
+  "productLink",
+  "productContextImg",
+  "productBackground",
+  "productImage",
+  "productDiscount",
+  "productPrice",
+  "productListPrice",
+  "productSellingPrice",
+];
+
+const ProductCard = ({
+  name,
+  url,
+  discount,
+  brand,
+  price,
+  sellingPrice,
+  image,
+}: ProductCard) => {
+  const handles = useCssHandles(CSS_HANDLES);
+
+  useEffect(() => {
+    console.log("isActive: ");
+  }, []);
+
+  return (
+    <div className={handles.productContainer}>
+      <div className={handles.productContextText}>
+        <p className={handles.productText}>
+          {brand && <span className={handles.productbrand}>{brand}</span>}
+          {name}
+          {discount && (
+            <span className={handles.productDiscount}>{discount}</span>
+          )}
+        </p>
+        {sellingPrice && (
+          <p
+            className={`${handles.productPrice} ${handles.productSellingPrice}`}
+          >
+            {sellingPrice}
+            {price && (
+              <span
+                className={`${handles.productPrice} ${handles.productListPrice}`}
+              >
+                {price}
+              </span>
+            )}
+          </p>
+        )}
+        <a href={url} className={handles.productLink}>
+          Comprar
+        </a>
+      </div>
+      <div className={handles.productContextImg}>
+        <div className={handles.productBackground}>
+          <img className={handles.productImage} src={image} alt="img" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProductCard.schema = {
+  title: "ProductCard",
+  description: "ProductCard props",
+  type: "object",
+  properties: {
+    name: {
+      title: "Nombre del Producto",
+      type: "string",
+    },
+    price: {
+      title: "Precio sin descuento",
+      type: "string",
+    },
+    sellingPrice: {
+      title: "Precio con descuento",
+      type: "string",
+    },
+    brand: {
+      title: "Marca del Producto",
+      type: "string",
+    },
+    discount: {
+      title: "Porcentaje de descuento",
+      type: "string",
+    },
+    callToAction: {
+      title: "Boton de compra",
+      type: "string",
+    },
+    url: {
+      title: "Url del Producto",
+      type: "string",
+    },
+    image: {
+      title: "Imagen del Producto",
+      type: "string",
+      widget: {
+        "ui:widget": "image-uploader",
+      },
+    },
+  },
+};
+
+export default ProductCard;
--- react/typings/storefront.d.ts
+++ react/typings/storefront.d.ts
@@ -1,18 +1,32 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent } from "react";
 
 declare global {
   interface StorefrontFunctionComponent<P = GenericObject>
     extends FunctionComponent<P> {
-    getSchema?(props: P): GenericObject
-    schema?: GenericObject
+    getSchema?(props: P): GenericObject;
+    schema?: GenericObject;
   }
 
   interface StorefrontComponent<P = GenericObject, S = GenericObject>
     extends Component<P, S> {
-    getSchema?(props: P): GenericObject
-    schema: GenericObject
+    getSchema?(props: P): GenericObject;
+    schema: GenericObject;
   }
+
   interface PromotionalCountdown {
     isActive: boolean;
+    children?: Array<Exclude<ReactNode, boolean | null | undefined>>;
+    date: string;
+  }
+
+  interface ProductCard {
+    name: string;
+    price: string;
+    sellingPrice: string;
+    brand: string;
+    discount: string;
+    callToAction: string;
+    url: string;
+    image: string;
   }
 }
